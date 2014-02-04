e02e1fac3f3f5fee40ddb914c4d3a6528d54b827
Marten Veldthuis <marten@veldthuis.com>
2014-02-04 16:41:49 +0100

    Fix tests

--- spec/roqua/support/errors_spec.rb
+++ spec/roqua/support/errors_spec.rb
@@ -25,19 +25,30 @@ describe 'Error reporting' do
   end
 
   it 'sends notifications to airbrake' do
-    stub_const("Airbrake", double("Airbrake"))
+    stub_const("Airbrake", double("Airbrake", is_ignored_exception?: false))
     Airbrake.should_receive(:notify_or_ignore).with(exception, parameters: {})
     Roqua::Support::Errors.report exception
   end
 
-  it 'sends notifications to appsignal' do
-    stub_const("Appsignal", double("Appsignal"))
-    Appsignal.should_receive(:send_exception_with_tags).with(exception, parameters: {})
-    Roqua::Support::Errors.report exception
+  context 'when Appsignal is loaded' do
+    let(:agent)       { double("agent") }
+    let(:transaction) { double("transaction") }
+
+    it 'sends notifications to appsignal' do
+      stub_const("Appsignal", Module.new)
+      Appsignal.stub(is_ignored_exception?: false, agent: agent)
+      stub_const("Appsignal::Transaction", double("Transaction", create: transaction))
+
+      transaction.should_receive(:set_tags).with({})
+      transaction.should_receive(:add_exception).with(exception)
+      transaction.should_receive(:complete!)
+      agent.should_receive(:send_queue)
+      Roqua::Support::Errors.report exception
+    end
   end
 
   it 'supports default extra params' do
-    Roqua::Support::Errors.extra_parameters = {organization: 'some_org'}
+    Roqua::Support::Errors.stub(extra_parameters: {organization: 'some_org'})
     Roqua.logger.should_receive(:error).with('roqua.exception',
                                              class_name: 'Exception',
                                              message: 'exception_message',

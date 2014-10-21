aab5a6131649169c28531dca1627672916141e8f
Henk <henk.van.der.veen@gmail.com>
2014-10-21 14:06:49 +0200

    Added active_interaction_aware_responder and api_errors_responder

---
+++ lib/roqua/responders/active_interaction_aware_responder.rb
@@ -0,0 +1,21 @@
+# For json and xml, will render the result of an active_interaction operation if no errors where present.
+# Otherwise renders the interaction
+module Roqua::Responders::ActiveInteractionAwareResponder
+  def to_format
+    if resource.is_a?(ActiveInteraction::Base)
+      if resource.errors.empty?
+        @resource = resource.result
+        @resources[-1] = resource
+      end
+    end
+    super
+  end
+
+  def json_resource_errors
+    if !resource.is_a?(ActiveInteraction::Base) || resource.errors.empty?
+      super
+    else
+      {:errors => resource.errors.as_json}
+    end
+  end
+end
---
+++ lib/roqua/responders/api_errors_responder.rb
@@ -0,0 +1,11 @@
+# Scopes json errors to the object-class:
+#
+# @example
+#   {errors: {person: name: ['blank']}}
+#
+# Does not play well with others, replaces json_resource_errors; when including multiple responders, ordering matters.
+module Roqua::Responders::ApiErrorsResponder
+  def json_resource_errors
+    {:errors => {resource.class.name.underscore => resource.errors}}
+  end
+end

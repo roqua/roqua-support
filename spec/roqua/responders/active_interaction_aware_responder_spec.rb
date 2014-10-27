7beae934e9b823f3ed2a94bc15954e742004ca13
Henk <henk.van.der.veen@gmail.com>
2014-10-27 16:15:00 +0100

    uncommented some code

--- spec/roqua/responders/active_interaction_aware_responder_spec.rb
+++ spec/roqua/responders/active_interaction_aware_responder_spec.rb
@@ -1,58 +1,58 @@
 require "spec_helper"
 
-# require 'roqua/responders/active_interaction_aware_responder'
-# class AIAResponder < ActionController::Responder
-#   include Roqua::Responders::ActiveInteractionAwareResponder
-# end
+require 'roqua/responders/active_interaction_aware_responder'
+class AIAResponder < ActionController::Responder
+  include Roqua::Responders::ActiveInteractionAwareResponder
+end
 
-# require 'ostruct'
-# require 'active_interaction'
-# class TestInteraction < ActiveInteraction::Base
-#   string :some_string
-#   def execute
-#     {foo: some_string}
-#   end
-# end
+require 'ostruct'
+require 'active_interaction'
+class TestInteraction < ActiveInteraction::Base
+  string :some_string
+  def execute
+    {foo: some_string}
+  end
+end
 
-# class ApplicationController < ActionController::Base
-#   respond_to :json
-# end
+class ApplicationController < ActionController::Base
+  respond_to :json
+end
 
-# describe Roqua::Responders::ActiveInteractionAwareResponder, type: :controller do
-#   let(:valid_interaction)   { TestInteraction.run some_string: 'bla' }
-#   let(:invalid_interaction) { TestInteraction.run }
+describe Roqua::Responders::ActiveInteractionAwareResponder, type: :controller do
+  let(:valid_interaction)   { TestInteraction.run some_string: 'bla' }
+  let(:invalid_interaction) { TestInteraction.run }
 
-#   context 'with a valid interaction' do
-#     controller do
-#       self.responder  = AIAResponder
-#       def index
-#         use_case = TestInteraction.run some_string: 'bla'
-#         respond_with use_case
-#       end
-#     end
+  context 'with a valid interaction' do
+    controller do
+      self.responder  = AIAResponder
+      def index
+        use_case = TestInteraction.run some_string: 'bla'
+        respond_with use_case
+      end
+    end
 
-#     subject { get :index, format: :json }
+    subject { get :index, format: :json }
 
-#     it 'returns the result if valid' do
-#       subject
-#       expect(response.body).to eq '{"foo":"bla"}'
-#     end
+    it 'returns the result if valid' do
+      subject
+      expect(response.body).to eq '{"foo":"bla"}'
+    end
 
-#   end
+  end
 
-#   context 'with an invalid interaction' do
-#     controller do
-#       self.responder  = AIAResponder
-#       def index
-#         use_case = TestInteraction.run
-#         respond_with use_case
-#       end
-#     end
-#     subject { post :index, format: :json }
+  context 'with an invalid interaction' do
+    controller do
+      self.responder  = AIAResponder
+      def index
+        use_case = TestInteraction.run
+        respond_with use_case
+      end
+    end
+    subject { post :index, format: :json }
 
-#     it 'returns the interaction' do
-#       subject
-#       expect(response.body).to eq '{"errors":{"some_string":["is required"]}}'
-#     end
-#   end
-# end
+    it 'returns the interaction' do
+      subject
+      expect(response.body).to eq '{"errors":{"some_string":["is required"]}}'
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,7 @@ require 'bundler/setup'
 
 require 'combustion'
 
-Combustion.schema_format = :sql
 Combustion.initialize! :action_controller
 
-module ActiveRecord
-  module VERSION
-    STRING = '4'
-  end
-end
+module ActiveRecord; module VERSION; STRING = '4'; end; end # needed since one test defines ActiveRecord making next line think it's actually loaded.
 require 'rspec/rails'

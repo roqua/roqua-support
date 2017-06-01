3fe10700c8365967422029c14dd7bd06bf479dca
Arnold van der Meulen <zwippie@gmail.com>
2017-06-01 11:32:22 +0200

    Fix incorrect instructions

--- lib/roqua/middleware/remove_hsts_header.rb
+++ lib/roqua/middleware/remove_hsts_header.rb
@@ -4,7 +4,7 @@ module Roqua
   module Middleware
     # Remove HSTS header because our loadbalancer will already add it instead.
     # Add the following line to config/application.rb in the Rails app:
-    #   config.middleware.use Roqua::RemoveHstsHeader
+    #   config.middleware.use Roqua::Middleware::RemoveHstsHeader
     class RemoveHstsHeader
       def initialize(app, _redirect: {}, _hsts: {}, _secure_cookies: true)
         @app = app
